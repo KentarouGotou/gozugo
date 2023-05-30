c07866c493195e4b6b614293d6d0f3fa2887da30
gozu <gotokentaro0611@keio.jp>
2023-05-30 22:02:51 +0900

    first commit

---
+++ src/calculator/sub.go
@@ -0,0 +1,11 @@
+package calculator
+
+var logMessage = "[LOG]"
+
+// Version of the calculator
+var Version = "1.0"
+
+// Sum two integer numbers
+func Sub(number1, number2 int) int {
+	return number1 - number2
+}
---
+++ src/helloworld/go.mod
@@ -0,0 +1,7 @@
+module helloworld
+
+go 1.20
+
+require github.com/KentarouGotou/calculator v0.0.0
+
+replace github.com/KentarouGotou/calculator => ../calculator
