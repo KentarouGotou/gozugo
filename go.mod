51619f9df60f059c78d60eb13eb894f376b6f2be
KentarouGotou <92863589+KentarouGotou@users.noreply.github.com>
2023-07-20 20:19:11 +0900

    simpleAPI

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/KentarouGotou/gozugo
 
 go 1.20
 
-require rsc.io/quote v1.5.2
-
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+    
+)
--- src/helloworld/main.go
+++ src/helloworld/main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
 
-	"github.com/KentarouGotou/gozugo/src/calculator"
+	"github.com/gorilla/mux"
 )
 
+// Message structはレスポンスとして返すメッセージを表します。
+type Message struct {
+	Text string `json:"message"`
+}
+
+// メインのハンドラ関数
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	// メッセージを作成
+	message := Message{Text: "Hello, World!"}
+
+	// レスポンスをJSON形式で返す
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(message)
+}
+
 func main() {
-	total := calculator.Sum(3, 5)
-	fmt.Println(total)
-	fmt.Println("Version: ", calculator.Version)
+	// ルータを作成
+	r := mux.NewRouter()
+
+	// ハンドラを設定
+	r.HandleFunc("/hello", helloHandler).Methods("GET")
+
+	// サーバを開始
+	log.Println("APIサーバをポート8080で起動中...")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
