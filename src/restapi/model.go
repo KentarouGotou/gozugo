6f77dd6b32cc0eeeae30a6a58e8866674f2f34d2
KentarouGotou <92863589+KentarouGotou@users.noreply.github.com>
2023-08-14 01:48:11 +0900

    make func getnotes

--- src/restapi/model.go
+++ src/restapi/model.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -20,86 +21,74 @@ type Tab struct {
 	Tempo  int    `json:"tempo"`
 }
 
-var tabs = []Tab{
-	{
-		Id: 1,
-		Artist: "Beatles", 
-		Title: "Let it be", 
-		Notes: []Note{
-			{
-				Position: [6]string{"-", "1", "0", "2", "3", "-"}, 
-				Ties: [6]string{"-", "-", "-", "-", "-", "-"}, 
-				Effects: [6]string{"-", "vib", "-", "-", "-", "-"}, 
-				Duration: 960
-			},
-			{
-				Position: [6]string{"-", "1", "-", "-", "-", "-"}, 
-				Ties: [6]string{"-", "t", "-", "-", "-", "-"}, 
-				Effects: [6]string{"-", "vib", "-", "-", "-", "-"},
-				Duration: 960
-			}
-			}, 
-		Tempo: 120
-	},
-	{
-		Id: 1, 
-		Artist: "Beatles", 
-		Title: "Let it be",
-		 Notes: []Note{
-			{
-				Position: [6]string{"-", "-", "-", "7", "-", "-"}, 
-				Ties: [6]string{"-", "-", "-", "-", "-", "-"}, 
-				Effects: [6]string{"-", "-", "-", "bn2", "-", "-"}, 
-				Duration: 480
-			},
-			{
-				Position: [6]string{"-", "-", "-", "9", "10", "-"}, 
-				Ties: [6]string{"-", "-", "-", "t", "-", "-"}, 
-				Effects: [6]string{"-", "-", "-", "-", "-", "-"}, 
-				Duration: 480
-			}
-		}, 
-		Tempo: 120
-	},
-}
+// var tabs = []Tab{
+// 	{
+// 		Id: 1,
+// 		Artist: "Beatles",
+// 		Title: "Let it be",
+// 		Notes: []Note{
+// 			{
+// 				Position: [6]string{"-", "1", "0", "2", "3", "-"},
+// 				Ties: [6]string{"-", "-", "-", "-", "-", "-"},
+// 				Effects: [6]string{"-", "vib", "-", "-", "-", "-"},
+// 				Duration: 960
+// 			},
+// 			{
+// 				Position: [6]string{"-", "1", "-", "-", "-", "-"},
+// 				Ties: [6]string{"-", "t", "-", "-", "-", "-"},
+// 				Effects: [6]string{"-", "vib", "-", "-", "-", "-"},
+// 				Duration: 960
+// 			}
+// 			},
+// 		Tempo: 120
+// 	},
+// 	{
+// 		Id: 1,
+// 		Artist: "Beatles",
+// 		Title: "Let it be",
+// 		 Notes: []Note{
+// 			{
+// 				Position: [6]string{"-", "-", "-", "7", "-", "-"},
+// 				Ties: [6]string{"-", "-", "-", "-", "-", "-"},
+// 				Effects: [6]string{"-", "-", "-", "bn2", "-", "-"},
+// 				Duration: 480
+// 			},
+// 			{
+// 				Position: [6]string{"-", "-", "-", "9", "10", "-"},
+// 				Ties: [6]string{"-", "-", "-", "t", "-", "-"},
+// 				Effects: [6]string{"-", "-", "-", "-", "-", "-"},
+// 				Duration: 480
+// 			}
+// 		},
+// 		Tempo: 120
+// 	},
+// }
 
 var Db *sql.DB
+
 func init() {
 	var err error
-	DB, err = sql.Open("sqlite3", "./example.sqlite")
+	Db, err = sql.Open("sqlite3", "./example.sqlite")
 	if err != nil {
 		panic(err)
 	}
 }
 
-func getTabs(limit int) (tabs []Tab, err error) {
-	stmt := "SELECT id, artist, title, tempo FROM tabs LIMIT $1"
-	rows, err := Db.Query(stmt, limit)
+func GetNotesforTab(id int) []Note {
+	rows, err := Db.Query("select * from notes where tab_id = ?", id)
 	if err != nil {
-		return
+		panic(err)
 	}
+	defer rows.Close()
 
+	var notes []Note
 	for rows.Next() {
-		tab := Tab{}
-		err = rows.Scan(&tab.Id, &tab.Artist, &tab.Title, &tab.Notes, &tab.Tempo)
+		var note Note
+		err := rows.Scan(&note.Position, &note.Ties, &note.Effects, &note.Duration)
 		if err != nil {
-			return
+			panic(err)
 		}
-		tabs = append(tabs, tab)
+		notes = append(notes, note)
 	}
-	rows.Close()
-	return
-}
-
-func getTab(id int) (tab Tab, err error) {
-	tab = Tab{}
-	stmt := "SELECT id, artist, title, tempo FROM tabs WHERE id = $1"
-	err = Db.QueryRow(stmt, id).Scan(&tab.Id, &tab.Artist, &tab.Title, &tab.Notes, &tab.Tempo)
-	return
-}
-
-func (tab *Tab) createTab() (err error) {
-	stmt := "INSERT INTO tabs (artist, title, tempo) VALUES ($1, $2, $3) RETURNING id"
-	err = Db.QueryRow(stmt, tab.Artist, tab.Title, tab.Tempo).Scan(&tab.Id)
-	return
+	return notes
 }
